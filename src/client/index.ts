99e019d7dd443fef0bba312da8b969136b558bd5
Nick <hoernernick@web.de>
2026-04-22 13:48:53 +0200

    clients now have a REPL loop

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,7 +1,16 @@
 import amqp from "amqplib";
-import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+import {
+  clientWelcome,
+  commandStatus,
+  getInput,
+  printClientHelp,
+  printQuit,
+} from "../internal/gamelogic/gamelogic.js";
 import { declareAndBind, SimpleQueueType } from "../internal/pubsub/publish.js";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import { GameState } from "../internal/gamelogic/gamestate.js";
+import { commandSpawn } from "../internal/gamelogic/spawn.js";
+import { commandMove } from "../internal/gamelogic/move.js";
 
 async function main() {
   const rabbitCon = `amqp://guest:guest@localhost:5672/`;
@@ -13,15 +22,46 @@ async function main() {
 
   const username = await clientWelcome();
 
-  declareAndBind(
+  const [channel, queue] = await declareAndBind(
     conn,
     ExchangePerilDirect,
     `${PauseKey}.${username}`,
     PauseKey,
     SimpleQueueType.Transient,
   );
-
   console.log("Starting Peril client...");
+
+  let gs = new GameState(username);
+
+  while (true) {
+    const inputs = await getInput();
+    if (inputs.length === 0) {
+      continue;
+    }
+
+    let firstWord = inputs[0];
+
+    if (firstWord === "spawn") {
+      commandSpawn(gs, inputs);
+    } else if (firstWord === "move") {
+      const move = commandMove(gs, inputs);
+      if (move) {
+        console.log("move successful");
+      }
+    } else if (firstWord === "status") {
+      commandStatus(gs);
+    } else if (firstWord === "help") {
+      printClientHelp();
+    } else if (firstWord === "spam") {
+      console.log("Spamming is not allowed yet!");
+    } else if (firstWord === "quit") {
+      printQuit();
+      break;
+    } else {
+      console.log("Error: command not found");
+      continue;
+    }
+  }
 }
 
 main().catch((err) => {
--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,7 @@ import amqp from "amqplib";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
 import { publishJSON } from "../internal/pubsub/publish.js";
 import type { PlayingState } from "../internal/gamelogic/gamestate.js";
+import { getInput, printServerHelp } from "../internal/gamelogic/gamelogic.js";
 
 async function main() {
   const rabbitCon = `amqp://guest:guest@localhost:5672/`;
@@ -16,9 +17,32 @@ async function main() {
     isPaused: true,
   };
 
-  await publishJSON(confChannel, ExchangePerilDirect, PauseKey, state);
+  // await publishJSON(confChannel, ExchangePerilDirect, PauseKey, state);
 
   console.log("Starting Peril server...");
+
+  printServerHelp();
+
+  while (true) {
+    const inputs = await getInput();
+    if (inputs.length === 0) {
+      continue;
+    } else if (inputs[0] === "pause") {
+      console.log("Sending a pause message");
+      await publishJSON(confChannel, ExchangePerilDirect, PauseKey, state);
+    } else if (inputs[0] === "resume") {
+      console.log("Sending a resume message");
+      await publishJSON(confChannel, ExchangePerilDirect, PauseKey, {
+        isPaused: false,
+      });
+    } else if (inputs[0] === "quit") {
+      console.log("Quitting");
+      break;
+    } else {
+      console.log("Dindn't understood the command");
+      continue;
+    }
+  }
 
   process.on("SIGINT", (code) => {
     console.log("Shuting down");
